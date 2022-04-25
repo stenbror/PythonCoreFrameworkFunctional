7e1a302f04180c5814e82761143bdd5f0dce4e5a
Richard Magnor Stenbro <stenbror@hotmail.com>
2022-04-25 20:06:44 +0200

    Added Name, Number and String with UnitTests.

--- PythonCoreFrameworkFunctional/PythonCoreParser.fs
+++ PythonCoreFrameworkFunctional/PythonCoreParser.fs
@@ -1,21 +1,28 @@
 namespace PythonCoreFrameworkFunctional
 
-
 type Trivia =
     |   Empty
 
 type Token =
     |   Empty
+    |   EOF of uint
     |   PyFalse of uint * uint * Trivia array
     |   PyNone of uint * uint * Trivia array
     |   PyTrue of uint * uint * Trivia array
     |   PyEllipsis of uint * uint * Trivia array
+    |   Name of uint * uint * string * Trivia array
+    |   Number of uint * uint * string * Trivia array
+    |   String of uint * uint * string * Trivia array
     static member GetStartPosition(symbol: Token) : uint =
         match symbol with
+        |   EOF(s)
         |   PyFalse(s, _ , _ )
         |   PyNone(s, _ , _ )
         |   PyTrue(s, _ , _ )
-        |   PyEllipsis(s, _ , _) -> s
+        |   PyEllipsis(s, _ , _)
+        |   Name(s, _, _, _ )
+        |   Number(s, _ , _ , _ )
+        |   String(s, _ , _ , _) -> s
         |   _   ->  0u
     
 type ASTNode =
@@ -24,6 +31,9 @@ type ASTNode =
     |   None of uint * uint * Token
     |   True of uint * uint * Token
     |   Ellipsis of uint * uint * Token
+    |   Name of uint * uint * Token
+    |   Number of uint * uint * Token
+    |   String of uint * uint * Token array
     
    
 type TokenStream = Token list
@@ -37,7 +47,10 @@ module PythonCoreParser =
         |   symbol :: rest ->   Some(symbol, rest)
         |   _ ->    Option.None
         
-    let GetStartPosition(stream: TokenStream) : uint = Token.GetStartPosition(stream.Head)
+    let GetStartPosition(stream: TokenStream) : uint =
+            if stream.Length > 0 then
+                Token.GetStartPosition(stream.Head)
+            else 0u
 
     let rec ParseAtom(stream: TokenStream) : (ASTNode * TokenStream) =
         match TryToken stream with
@@ -53,4 +66,25 @@ module PythonCoreParser =
         |   Some(Token.PyEllipsis(s, e, _ ), rest) ->
                 let op = List.head stream
                 ASTNode.Ellipsis(s, e, op), rest
+        |   Some(Token.Name(s, e, _  , _ ), rest) ->
+                let op = List.head stream
+                ASTNode.Name(s, e, op), rest
+        |   Some(Token.Number(s, e, _  , _ ), rest) ->
+                let op = List.head stream
+                ASTNode.Number(s, e, op), rest
+        |   Some(Token.String(s, e, _ , _ ), rest) ->
+                let spanStart = GetStartPosition(stream)
+                let mutable restAgain = rest
+                let mutable nodes : Token list = List.Empty
+                nodes <- stream.Head :: nodes
+                while   restAgain.Length > 0 &&
+                                match TryToken restAgain with
+                                |  Some(Token.String(s, e, _ , _ ), restNow) ->
+                                        nodes <- restAgain.Head :: nodes
+                                        restAgain <- restNow
+                                        true
+                                |       Option.None -> false
+                                |       _ ->    false
+                        do ()
+                ASTNode.String(spanStart, GetStartPosition(restAgain), List.toArray(List.rev nodes)), restAgain
         |   _   ->  raise ( SyntaxError(GetStartPosition(stream), "Expecting an atom literal!") )
--- TestPythonCoreFrameworkFunctional/TestPythonCoreExpressionRules.fs
+++ TestPythonCoreFrameworkFunctional/TestPythonCoreExpressionRules.fs
@@ -39,3 +39,42 @@ module ExpressionsRulesTests =
          
          Assert.Equal(node, ASTNode.Ellipsis(0u, 3u, Token.PyEllipsis(0u, 3u, [|  |])))
          Assert.True(rest.Length = 0)
+         
+    [<Fact>]
+    let ``Test Atom Expression rule for name token``() =
+         let stream = [ Token.Name(0u, 3u, "abc", [|  |]); ]
+         let node, rest = PythonCoreParser.ParseAtom stream
+         
+         Assert.Equal(node, ASTNode.Name(0u, 3u, Token.Name(0u, 3u, "abc", [|  |])))
+         Assert.True(rest.Length = 0)
+         
+    [<Fact>]
+    let ``Test Atom Expression rule for number token``() =
+         let stream = [ Token.Name(0u, 4u, "1.34", [|  |]); ]
+         let node, rest = PythonCoreParser.ParseAtom stream
+         
+         Assert.Equal(node, ASTNode.Name(0u, 4u, Token.Name(0u, 4u, "1.34", [|  |])))
+         Assert.True(rest.Length = 0)
+         
+    [<Fact>]
+    let ``Test Atom Expression rule for single string token``() =
+         let stream = [ Token.String(0u, 14u, "'Hello, World!'", [|  |]); Token.EOF(14u) ]
+         let node, rest = PythonCoreParser.ParseAtom stream
+         
+         Assert.Equal(node, ASTNode.String(0u, 14u, [| Token.String(0u, 14u, "'Hello, World!'", [|  |]) |] ))
+         Assert.True(rest.Length = 1)
+         
+    [<Fact>]
+    let ``Test Atom Expression rule for multiple string token``() =
+         let stream = [
+              Token.String(0u, 14u, "'Hello, World!'", [|  |])
+              Token.String(14u, 25u, "'bye, bye!'", [|  |])
+              Token.EOF(25u)
+         ]
+         let node, rest = PythonCoreParser.ParseAtom stream
+         
+         Assert.Equal(node, ASTNode.String(0u, 25u, [|
+              Token.String(0u, 14u, "'Hello, World!'", [|  |])
+              Token.String(14u, 25u, "'bye, bye!'", [|  |])
+         |] ))
+         Assert.True(rest.Length = 1)
